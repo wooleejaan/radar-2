0fdf454b3170af0af6e62be11d3f8198ef9f1614
wooleejaan <wooleejaan@gmail.com>
2023-08-09 10:47:09 +0900

    :recycle: Axis 조정

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Radar from "@/components/radar/radar";
 
 const data = [
-  { axis: "strength", value: 13, order: 0 },
-  { axis: "intelligence", value: 1, order: 1 },
-  { axis: "charisma", value: 8, order: 2 },
+  { axis: "strength", value: 10, order: 0 },
+  { axis: "intelligence", value: 2, order: 1 },
+  { axis: "charisma", value: 6, order: 2 },
   { axis: "dexterity", value: 4, order: 3 },
   { axis: "luck", value: 10, order: 4 },
   // { axis: "azole", value: 10, order: 5 },
